c6691e5b564115d9f04c9c33e87084b0af9159ee
Volodymyr Ukraynets <vuk000@post.uit.no>
2017-12-11 13:12:15 +0200

    init new branch

--- src/main/java/spring/start/Main.java
+++ src/main/java/spring/start/Main.java
@@ -2,12 +2,6 @@ package spring.start;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import spring.POJO.robot.Robot;
-import spring.interfaces.part.body.robot.RobotConveyor;
-import spring.pool.TerminatorPool;
-import spring.robot.Terminator;
-
-import java.io.File;
 
 
 /**
@@ -16,12 +10,12 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        Terminator terminator = (Terminator) context.getBean("model1");
-        Terminator terminator1 = (Terminator) context.getBean("model2");
+//        Terminator terminator = (Terminator) context.getBean("model1");
+//        Terminator terminator1 = (Terminator) context.getBean("model2");
 
 
-        terminator.action();
-        terminator1.action();
+//        terminator.action();
+//        terminator1.action();
 //        ApplicationContext context = new ClassPathXmlApplicationContext("all_context.xml");
 
 //        RobotConveyor terConveyor = (RobotConveyor)context.getBean("terConveyor");
